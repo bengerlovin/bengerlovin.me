26dd92b672aac9fbcceecf3782cf9228ed5f5c18
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-02-09 13:12:38 -0500

    footer edits

--- components/Navigation/Footer.tsx
+++ components/Navigation/Footer.tsx
@@ -4,12 +4,18 @@ export default function Footer() {
   return (
     <>
       <footer className='flex flex-col justify-center px-8'>
-        <div className='relative flex flex-col items-start w-full max-w-3xl py-8 mx-auto'>
+        <div className='relative items-start w-full max-w-3xl py-8 mx-auto'>
           {/* HR Separator */}
           <hr className='bg-gray-400 border-gray-500'></hr>
 
-          <div>hello</div>
-          <div>hello</div>
+          <div className='flex items-center justify-between max-w-3xl py-6'>
+            <div className='flex-shrink-0 w-1/4'>hello</div>
+            <div className='flex-shrink-0 w-1/4'>hello</div>
+            <div className='flex-shrink-0 w-1/4'>
+              hello friendsssssss this is another bit of text
+            </div>
+            <div className='flex-shrink-0 w-1/4'>hello</div>
+          </div>
         </div>
       </footer>
     </>
