aebba222040a5cea852164cace148f91c24d93f0
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-02-15 14:55:06 -0500

    small fixes, added swr config globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/global.css';
+import { SWRConfig } from 'swr'
+import fetcher from 'lib/fetcher'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SWRConfig value={{ fetcher: fetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -91,7 +91,6 @@ export default function Home({ commitCount, playbackTime, youTubeStats }) {
 export async function getStaticProps() {
 
   let ytStats: number = await getYoutubeStats();
-  console.log(ytStats)
   let recentPlaybackTime = await getSpotifyPlayHistory();
 
   let commitData: GithubCommitDataResult = await getRecentCommitData();
