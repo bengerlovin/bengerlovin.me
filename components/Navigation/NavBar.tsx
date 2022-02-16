97ffeefda8228ffaba47a10f5da15186d49ca71b
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-02-16 14:11:00 -0500

    nav bar and homepage adjustments

--- components/Navigation/NavBar.tsx
+++ components/Navigation/NavBar.tsx
@@ -14,12 +14,12 @@ export default function Navigation() {
           <Logo />
 
           {/* Laptop Screen Menu List */}
-          <div className='hidden md:flex gap-x-6'>
+          <div className='hidden md:flex md:items-center gap-x-6'>
 
             {/* Project Drop Down Menu */}
             <DropdownMenu>
-              <span className='flex gap-1 group items-center'>
-                <a className='text-md font-inter tracking-tight text-zinc-900 underline group-hover:text-blue-700 decoration-white transition-all duration-200 focus:ring-0 focus:outline-none outline-none' target={'_blank'}>Projects</a>
+              <span className='flex gap-1 group items-center transition-all duration-200'>
+                <a className='text-md font-inter tracking-tight text-zinc-900 underline group-hover:text-blue-700 decoration-white  focus:ring-0 focus:outline-none outline-none' target={'_blank'}>Projects</a>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-zinc-900  group-hover:text-blue-700  decoration-white" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
@@ -28,7 +28,7 @@ export default function Navigation() {
 
             {/* About */}
             <Link passHref href={'/about'}>
-              <a className='text-md font-inter tracking-tight text-zinc-900 underline hover:text-blue-700 hover:decoration-blue-700 decoration-white transition-all duration-200' target={'_blank'}>About</a>
+              <a className='text-md font-inter tracking-tight text-zinc-900 underline hover:text-blue-700 hover:decoration-blue-700 decoration-white transition-all duration-200' target={'_self'}>About</a>
             </Link>
 
             {/* Contact */}
