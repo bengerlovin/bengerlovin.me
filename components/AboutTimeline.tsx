3fd22a92bfe02219bc7fc7a84e7e331a0639947e
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-02-17 14:52:38 -0500

    about page timeline typo edits

--- components/AboutTimeline.tsx
+++ components/AboutTimeline.tsx
@@ -16,19 +16,19 @@ const AboutTimeline = ({ }) => {
                     <TimelineItem title="Discovering Programming" date="September, 2018">
                         <Text classes="mb-2">At my best friend's request, I take an introductory computer science course in Java. I don't think I'll enjoy it (spoilers, I do) since  I'm mostly there to hang out with my friends in computer science degrees. </Text>
 
-                        <Text classes="mb-3">I write my first "Hello World" program, and grow to appreciate computer science. At this point, I declare my degree as Bio-Organic Chemistry, and decide to pursue more CS courses as electives. </Text>
+                        <Text classes="mb-3">I write my first "Hello World" program and grow to appreciate computer science. At this point, I declare my degree as Bio-Organic Chemistry and decide to pursue more CS courses as electives. </Text>
                     </TimelineItem>
                 </li>
                 <li className="timeline-list-item">
                     <TimelineItem title="Pursuing Chemistry" date="May 2019">
-                        <Text classes="mb-2">I'm accepted into a prestigious pharmaceutical intership at Merck's headquarters in Kennilworth. I pack my bags and move to New Jersey for the summer of 2019. </Text>
+                        <Text classes="mb-2">I'm accepted into a prestigious pharmaceutical internship at Merck's headquarters in Kennilworth. I pack my bags and move to New Jersey for the summer of 2019. </Text>
 
                         <div className="flex flex-col w-full py-2 mt-4 mb-3 md:p-2">
                             <Image className="rounded-lg shadow-soft" height={500} width={1000} layout='responsive' placeholder='blur' src={merckOffices}></Image>
                             <caption className="mt-3 text-sm italic tracking-tight text-gray-600">Merck's offices in Kennilworth, NJ - my home for the summer of 2019</caption>
                         </div>
 
-                        <Text classes="mb-3"> I get to work with incredible chemists at Merck in their Drug Discovery division, and meet withseveral professors in New York whose labs I plan on applying in the fall to for PhD programs.</Text>
+                        <Text classes="mb-3"> I get to work with incredible chemists at Merck in their Drug Discovery division, and meet with several professors in New York whose labs I plan on applying in the fall to for PhD programs.</Text>
 
                     </TimelineItem>
                 </li>
@@ -36,7 +36,7 @@ const AboutTimeline = ({ }) => {
                     <TimelineItem title="Deciding Against A PhD" date="September 2019">
                         <Text classes="mb-2">Having finished my internship at Merck, I come to a crossroads. I feel grateful for my experiences in chemistry, but I can't shake the feeling that I don't want to work in another lab for 5 years, the average length of a PhD program.</Text>
 
-                        <Text classes="mb-3"> I decide to give myself a year to pursue tech as an alternative and see if I like working as a developer more than chemistry.</Text>
+                        <Text classes="mb-3"> I decide to give myself a year to pursue tech as an alternative and see if I like working as a developer more than as a chemist.</Text>
 
                     </TimelineItem>
                 </li>
@@ -59,7 +59,7 @@ const AboutTimeline = ({ }) => {
                             <caption className="mt-3 text-sm italic tracking-tight text-gray-600">I may be biased, but I think Montréal is one of the most beautiful and fun cities to exist and I was glad to start my tech career here</caption>
                         </div>
 
-                        <Text classes="mb-3">I was hired to help build the new version of my company's software, meaning a new tech stack (Vue.js and Java) and a completely refreshed UI. Full-stack paradise! </Text>
+                        <Text classes="mb-3">I was hired to help build the new version of my company's software, meaning a modern tech stack (Vue.js and Java) and a completely refreshed UI. Full-stack paradise! </Text>
 
                     </TimelineItem>
                 </li>
@@ -74,10 +74,10 @@ const AboutTimeline = ({ }) => {
                             And so, my <a target="_blank" href="https://happyhealthytechie.com" className="text-blue-800 underline transition-colors duration-200 ease-in-out hover:text-blue-400">developer blog</a> was born.
                         </Text>
 
-                        <div className="flex flex-col w-full py-2 mt-4 mb-5 shadow-soft-small md:p-2">
+                        <a target="_blank" href="https://happyhealthytechie.com" className="flex flex-col w-full py-2 mt-4 mb-5 transition-colors duration-200 ease-in-out border-[2.5px] border-white rounded-md shadow-soft-small md:p-2 hover:border-blue-500 cursor-pointer">
                             <Image className="rounded-lg" height={1000} width={1500} layout='responsive' placeholder='blur' src={hht} alt="montreal downtown"></Image>
 
-                        </div>
+                        </a>
 
 
                         <Text classes="mb-2">
--- pages/about.tsx
+++ pages/about.tsx
@@ -15,7 +15,7 @@ const About = () => {
                 <PageSubHeading>
                     About Me
                 </PageSubHeading>
-                <Text classes='mb-2'>I've had a winding path to get to my current job as a full-stack developer, espcially since I come from a background in organic chemistry and working in pharmaceutical companiess. </Text>
+                <Text classes='mb-2'>I've had a winding path to get to my current job as a full-stack developer, espcially since I come from a background in organic chemistry and working in pharmaceutical companies. </Text>
 
                 <Text classes='mb-10'>
                     I've made this little timeline to summarize my journey, starting with my first CS elective course in college.
