4d6d6d33372aca5e02fa2c6261fcf263d1fef760
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-02-17 11:37:35 -0500

    preload fonts fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,10 @@ export default function Home({ commitCount, playbackTime, youTubeStats }) {
       <Head>
         <title>Ben Gerlovin Portfolio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=Inter:wght@300;400;500;700&display=swap" rel="preload" as='stylesheet' />
+        <link rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin='' />
+        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=Inter:wght@300;400;500;700&display=swap" rel="preload" as='style' />
       </Head>
       <PageContainer>
 
