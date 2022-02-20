d48137306f0d16a1127362010885757ddecb6a40
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-02-20 08:48:56 -0500

    meta tag for index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home({ commitCount, playbackTime, youTubeStats }) {
       <Head>
         <title>Ben Gerlovin Portfolio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Personal portfolio site for Ben Gerlovin, full-stack developer, tech-blogger, cat-lover." />
         <link rel="preconnect"
           href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@800&family=Inter:wght@300;400;500;700&display=block" rel='stylesheet' />
